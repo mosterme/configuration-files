9221532f7b9bfa72846d8f1d380ce8a9c2547bb9
Michael Ostermeier <mosterme@users.noreply.github.com>
2023-10-21 23:48:46 +0200

    How to set the default text editor on Debian.

--- bin/readme.sh
+++ bin/readme.sh
@@ -6,6 +6,7 @@ echo "aliases, configurations, dotfiles, scripts and more."
 echo
 echo "* [What do you use to manage your dotfiles?](https://news.ycombinator.com/item?id=11070797)"
 echo "* [How to store dotfiles in a bare git repository.](https://www.atlassian.com/git/tutorials/dotfiles)"
+echo "* [How to set the default text editor](https://wiki.debian.org/TextEditor#How_to_set_a_default_text_editor) on Debian."
 echo
 
 for file in .*
--- readme.md
+++ readme.md
@@ -4,6 +4,7 @@ aliases, configurations, dotfiles, scripts and more.
 
 * [What do you use to manage your dotfiles?](https://news.ycombinator.com/item?id=11070797)
 * [How to store dotfiles in a bare git repository.](https://www.atlassian.com/git/tutorials/dotfiles)
+* [How to set the default text editor](https://wiki.debian.org/TextEditor#How_to_set_a_default_text_editor) on Debian.
 
 ## .aliases
 ```sh
